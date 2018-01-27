8071a75695d2c01fe67cd341c7f41f32e0c7fcae
DANNYCOM\DannyChan <chunc59@yahoo.com>
2018-01-27 19:22:30 +0800

    Added Titan Steel.

---
+++ OverKill/Items/Materials/TitanSteel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace OverKill.Items.Materials
+{
+    public class TitanSteel : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Titan Steel");
+            Tooltip.SetDefault("'Leviathan's Plate, you must be mad.'");
+            DisplayName.AddTranslation(GameCulture.Spanish, "Titan Steel");
+            Tooltip.AddTranslation(GameCulture.Spanish, "'Plato de Leviathan, debes estar enojado.'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 40;
+            item.height = 32;
+            item.value = 345000;
+            item.rare = 6;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType("SuperSteel"), 10);
+            recipe.AddIngredient(1198, 30);
+            recipe.AddTile(133);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
